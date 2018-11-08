6ba4b547d979c0f523301c5a53b7203693e8761c
Seyed Kamal Hosseini <sayarezamin89@gmail.com>
2018-11-09 01:00:45 +0330

    Add_notifications

--- app/src/main/java/com/example/kamal/saatzanhamrah/ForgotPackage/ForgotModel.java
+++ app/src/main/java/com/example/kamal/saatzanhamrah/ForgotPackage/ForgotModel.java
@@ -39,7 +39,8 @@ public class ForgotModel {
             public Map onMapPost() {
                 Map<String, String> serverParams = new HashMap<>();
                 serverParams.put("email",email);
-                serverParams.put("key_text_android", "ktaa");
+                serverParams.put("key_text_android","ktaa");
+
                 return serverParams;
             }
         });
